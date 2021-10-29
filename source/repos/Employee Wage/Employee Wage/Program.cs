378266dc8fde1a2d87f25e7a1ed13ca81e664c8f
Akhila4144 <orugantiakhila34@gmail.com>
2021-10-29 11:59:34 +0530

    Use case one for Employee caluculation

---
+++ source/repos/Employee Wage/Employee Wage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck==isFullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                 Console.ReadKey();
+            }
+        }
+    }
+}
